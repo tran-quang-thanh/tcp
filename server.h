0ad2ac9b783d40ee5d0c56b351921a63fbdfaae2
tran-quang-thanh <tranquangthanh191@gmail.com>
2024-10-19 15:37:28 +0800

    Create header file for server

---
+++ server.h
@@ -0,0 +1,8 @@
+#ifndef CLIENT_H
+#define CLIENT_H
+
+#include "headsock.h"
+
+void str_ser(int sockfd);
+
+#endif
--- tcp_ser.c
+++ tcp_ser.c
@@ -3,11 +3,9 @@ tcp_ser.c: the source file of the server in tcp transmission
 ***********************************/
 
 
-#include "headsock.h"
+#include "server.h"
 
 #define BACKLOG 10
-
-void str_ser(int sockfd);                                                        // transmitting and receiving function
 
 int main(void)
 {
@@ -65,48 +63,4 @@ int main(void)
 	}
 	close(sockfd);
 	exit(0);
-}
-
-void str_ser(int sockfd)
-{
-	char buf[BUFSIZE];
-	FILE *fp;
-	char recvs[DATALEN];
-	struct ack_so ack;
-	int end, n = 0;
-	long lseek=0;
-	end = 0;
-	
-	printf("receiving data!\n");
-
-	while(!end)
-	{
-		if ((n= recv(sockfd, &recvs, DATALEN, 0))==-1)                                   //receive the packet
-		{
-			printf("error when receiving\n");
-			exit(1);
-		}
-		if (recvs[n-1] == '\0')									//if it is the end of the file
-		{
-			end = 1;
-			n --;
-		}
-		memcpy((buf+lseek), recvs, n);
-		lseek += n;
-	}
-	ack.num = 1;
-	ack.len = 0;
-	if ((n = send(sockfd, &ack, 2, 0))==-1)
-	{
-			printf("send error!");								//send the ack
-			exit(1);
-	}
-	if ((fp = fopen ("myTCPreceive.txt","wt")) == NULL)
-	{
-		printf("File doesn't exit\n");
-		exit(0);
-	}
-	fwrite (buf , 1 , lseek , fp);					//write data into file
-	fclose(fp);
-	printf("a file has been successfully received!\nthe total data received is %d bytes\n", (int)lseek);
 }
